c1ddff877859fa9d26734175a41de29544b66864
4thcalabash <751188973@qq.com>
2023-06-28 20:38:30 +0800

    add requirements.txt and xcpc board data downloader.py

---
+++ tools/board_downloader.py
@@ -0,0 +1,120 @@
+# %%
+import sys, os
+sys.path.append('..')
+
+# %%
+import requests, time, json
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from loader import load_from_xcpc_board_offline
+
+
+# %%
+template_ranklist_uri = 'https://board.xcpcio.com/icpc/{}/{}?type=%E5%AF%BC%E5%87%BA%E6%A6%9C%E5%8D%95'
+template_teams_uri = 'https://board.xcpcio.com/data//icpc/{}/{}/team.json?t={}'
+force_mode = False
+
+# %%
+def fetch_ranklist(season, site):
+    uri = template_ranklist_uri.format(season, site)
+    driver = webdriver.Chrome()
+    driver.get(uri)
+    time.sleep(2)
+    data_format = driver.find_element(
+        By.XPATH, '/html/body/center/div/div/div[6]/div/span[1]/input')
+    if data_format is None:
+        raise Exception('Cannot Find Data Format Selector')
+    data_format.send_keys('Rank JSON\n')
+    time.sleep(1)
+    generate = driver.find_element(By.XPATH, '/html/body/center/div/div/button')
+    if generate is None:
+        raise Exception('Cannot Find Generate Button')
+    generate.click()
+    time.sleep(1)
+    result = driver.find_element(By.XPATH, '/html/body/center/div/div/div[7]/span/textarea')
+    if result is None:
+        raise Exception('Cannot Find Result')
+    result = result.text
+    return result
+
+
+# %%
+def fetch_teams(season, site):
+    now_ticks = int(time.time())
+    uri = template_teams_uri.format(season, site, now_ticks)
+    response = requests.get(uri)
+    if response.status_code != 200:
+        print(response)
+        raise Exception('Cannot Fetch Teams')
+    return response.json()
+
+# %%
+def export_board(export_path, season, site):
+    ranklist_path = export_path + '\\ranklist.json'
+    teams_path = export_path + '\\teams.json'
+    
+    ranlist = fetch_ranklist('47th', 'ec-final')
+    if force_mode and not os.path.exists(export_path):
+        os.makedirs(export_path)
+        print('Created Output Path: {}'.format(ranklist_path))
+        
+    with open(ranklist_path, 'w', encoding='utf-8') as f:
+        f.write(ranlist)
+    print('Ranklist Exported Successfully')
+
+    teams = fetch_teams('47th', 'ec-final')
+    with open(teams_path, 'w', encoding='utf-8') as f:
+        json.dump(teams, f, ensure_ascii=False)
+    print('Teams Exported Successfully')
+
+    competition_name, results = load_from_xcpc_board_offline(ranklist_path, teams_path)
+    print(competition_name)
+
+    print('Team count: {}'.format(len(results)))
+    print('Champion: {}'.format(results[0]))
+    print('Official Champion: {}'.format(list(filter(lambda x: x.is_official, results))[0]))
+
+
+
+
+# %%
+
+if __name__ == '__main__':
+    # Usage: 
+    # output_folder is the folder to store the output files.
+    # season is the season of the competition. This should be the same as the category in the xcpc board website.
+    # site is the site of the competition. This should be the same as the site in the xcpc board website.
+    # --force is a boolean value. If it is true, the program will create the output_folder if it is not exist and overwrite the existing files.
+    # --help will print the usage.
+
+    #output help info
+    if len(sys.argv) == 1 or sys.argv[1] == '--help':
+        print('Usage: python board_downloader.py output_folder season site [--force]')
+        print('Example: python board_downloader.py ./board_data/47th/ec-final 47th ec-final --force')
+        exit(0)
+    
+    #validate arguments
+    if len(sys.argv) < 4: 
+        print('Invalid Arguments')
+        print('Use --help to see the usage')
+        exit(1)
+    
+    #parse arguments
+    output_folder = sys.argv[1]
+    season = sys.argv[2]
+    site = sys.argv[3]
+    force_mode = '--force' in sys.argv
+
+    if force_mode:
+        print('Force Mode Enabled')
+    
+    #export board
+    export_board(output_folder, season, site)
+        
+   
+
+    
+
+#export_board('./board_data/47th/ec-final', '47th', 'ec-final')
+
+
